32f16311839b1b2c0520c736700fa1105e008291
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-08-03 00:26:25 -0300

    Configure Amplify

--- adopt-a-pet/src/index.js
+++ adopt-a-pet/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
